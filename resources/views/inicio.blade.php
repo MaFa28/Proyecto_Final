4d16c2f663749fb340bfd7f05ebacfb6fe819cb7
Marco Morales <marcomorales.mm41@gmail.com>
2022-11-06 16:41:01 -0600

    Agregando CSS y JS

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/bootstrap.min.css', 'resources/js/bootstrap.min.js'])
     <title>Document</title>
 </head>
 <body>
