75a28206286d801c226c932fe8b49b1828a4710b
Marco Morales <marcomorales.mm41@gmail.com>
2022-11-11 13:10:57 -0600

    Agregando Middleware a controladores

--- app/Http/Controllers/CitaController.php
+++ app/Http/Controllers/CitaController.php
@@ -11,7 +11,7 @@ class CitaController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->except('index','show');
+        $this->middleware('auth');///->except('index','show');
         //only('create');
     }
     /**
